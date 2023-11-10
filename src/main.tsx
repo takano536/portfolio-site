da45c0b3fa5dde9bd5cb14f2f4123ea2bd846b8f
takano536 <ohnishi.takanori@gmail.com>
2023-11-11 00:48:30 +0900

    modify build dir

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <HashRouter>
+  <BrowserRouter basename="/portfolio-site/">
     <App />
-  </HashRouter>,
+  </BrowserRouter>,
 );
